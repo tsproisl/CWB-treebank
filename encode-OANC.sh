0e3793a2a7ff845a9cc48879590699fe8c850fa1
Thomas Proisl <thomas.proisl@fau.de>
2017-05-03 14:00:02 +0200

    Changed encoding scripts

--- encode-OANC.sh
+++ encode-OANC.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-data="/var/treebank.info/corpora/c4ed248c94805b33377000000"
+data="/data/treebank.info/corpora/c4ed248c94805b33377000000"
 regfile="c4ed248c94805b33377000000"
 name="C4ED248C94805B33377000000"
-registry="/var/treebank.info/registry"
-infile="/home/pandora/4ed248c94805b33377000000.xml"
+registry="/data/treebank.info/registry"
+infile="/data/treebank.info/upload/4ed248c94805b33377000000.xml"
 
 export CORPUS_REGISTRY="$registry"
 
-cwb-encode -d $data -f $infile -R "$registry/$regfile" -xsB -P pos -P lemma -P wc -P indep -P outdep -P root -S s:0+id+id_md5_base64+original_id -S text:0+id+original_id -0 corpus
+cwb-encode -d $data -f $infile -R "$registry/$regfile" -xsB -c utf8 -P lower -P pos -P lemma -P wc -P indep/ -P outdep/ -P root -S s:0+id+id_md5_base64+original_id -S text:0+id+original_id -0 corpus
 
 cwb-make -M 500 -V "$name"
--- encode-taz-test.sh
+++ encode-taz-test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-data="/var/treebank.info/corpora/c505487317a2853de55000000"
+data="/data/treebank.info/corpora/c505487317a2853de55000000"
 regfile="c505487317a2853de55000000"
 name="C505487317A2853DE55000000"
-registry="/var/treebank.info/registry"
-infile="/home/pandora/505487317a2853de55000000.xml"
+registry="/data/treebank.info/registry"
+infile="/data/treebank.info/upload/505487317a2853de55000000.xml"
 
 export CORPUS_REGISTRY="$registry"
 
-cwb-encode -d $data -f $infile -R "$registry/$regfile" -xsB -P pos -P lemma -P wc -P indep -P outdep -P root -S s:0+id+id_md5_base64+original_id -S text:0+id+original_id -0 corpus
+cwb-encode -d $data -f $infile -R "$registry/$regfile" -xsB -c utf8 -P lower -P pos -P lemma -P wc -P indep/ -P outdep/ -P root -S s:0+id+id_md5_base64+original_id -S text:0+id+original_id -0 corpus
 
 cwb-make -M 500 -V "$name"
